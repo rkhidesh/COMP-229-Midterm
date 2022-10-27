075455a2e6d5a7d7bdc88e1f109776be5720178d
Rebecca <rebec@DESKTOP-K7N6MO0>
2022-10-27 13:51:05 -0400

    Added the delete button and made it functional

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //local MongoDB deployment ->
+  //cloud MongoDB deployment ->
   URI: "mongodb+srv://new_user_31:oMh0vE5rJq8ZcdMu@midtermdb.varvohr.mongodb.net/product_store?retryWrites=true&w=majority",
 };
