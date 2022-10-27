33d499d42241891f2360bd51431d48b5e1a59e9c
Rebecca <rebec@DESKTOP-K7N6MO0>
2022-10-26 22:18:16 -0400

    mongoatlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  URI: "mongodb://127.0.0.1/product_store",
+  URI: "mongodb+srv://new_user_31:oMh0vE5rJq8ZcdMu@midtermdb.varvohr.mongodb.net/product_store?retryWrites=true&w=majority",
 };
